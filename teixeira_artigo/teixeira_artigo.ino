286eb6a8d98c639b5dd4e9c269c74dcef9189886
Larissa Farias <larissa.farias@ngi.com.br>
2018-06-20 17:56:00 -0300

    Added files used for comparison

---
+++ teixeira_artigo/teixeira_artigo.ino
@@ -0,0 +1,21 @@
+float cond = 0.0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  float val = analogRead(A0);
+  float rc = 0.8;
+  float resistencia = (rc * 1024.0) / float(val) - rc;
+  float cond = (1/resistencia) * 1000;
+
+      Serial.print("Cond: ");
+      Serial.print(cond, 4);
+      Serial.print(" mS cm");
+      Serial.print(" | Resistência: ");
+      Serial.print(resistencia);
+      Serial.println(" ");
+
+      delay(2000);
+}
